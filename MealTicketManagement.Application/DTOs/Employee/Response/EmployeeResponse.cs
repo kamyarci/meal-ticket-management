ff0caa8de9d8d6230e65800d030869c26dbfc45b
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:44:46 -0300

    feat: implementa EmployeeResponse DTO

---
+++ MealTicketManagement.Application/DTOs/Employee/Response/EmployeeResponse.cs
@@ -0,0 +1,5 @@
+﻿using MealTicketManagement.Domain.Enums;
+
+namespace MealTicketManagement.Application.DTOs.Employee;
+
+public record EmployeeResponse(Guid Id, string Name, string Cpf, Status Status, DateTime UpdatedAt);
