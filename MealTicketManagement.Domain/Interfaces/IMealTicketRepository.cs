d74b10e61bf73c5f4f314a8ec6d43ab86c769a03
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 16:48:51 -0300

    feat: adiciona interface IMealTicketRepository

---
+++ MealTicketManagement.Domain/Interfaces/IMealTicketRepository.cs
@@ -0,0 +1,8 @@
+﻿using MealTicketManagement.Domain.Entities;
+
+namespace MealTicketManagement.Domain.Interfaces;
+
+public interface IMealTicketRepository : IRepository<MealTicket, Guid>
+{
+    Task<IEnumerable<MealTicket>> GetByPeriodAsync(DateTime start, DateTime end);
+}
