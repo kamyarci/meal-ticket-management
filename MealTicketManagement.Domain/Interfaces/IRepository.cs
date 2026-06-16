8841ac103ae34749a8a5984e2ceeccbc91a95848
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 16:48:07 -0300

    feat: adiciona interface generica

---
+++ MealTicketManagement.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace MealTicketManagement.Domain.Interfaces;
+
+public interface IRepository<TEntity, TKey>
+{
+    Task<TEntity?> GetByIdAsync(TKey id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+}
