62e561bd93e277abbf9310ac0d9413c69d14e4fe
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:44:57 -0300

    feat: implementa MealTicketResponse DTO

---
+++ MealTicketManagement.Application/DTOs/MealTicket/Response/MealTicketResponse.cs
@@ -0,0 +1,11 @@
+﻿using MealTicketManagement.Domain.Enums;
+
+namespace MealTicketManagement.Application.DTOs.MealTicket;
+
+public record MealTicketResponse(
+    Guid Id,
+    Guid EmployeeId,
+    string EmployeeName,
+    int Quantity,
+    Status Status,
+    DateTime DeliveredAt);
