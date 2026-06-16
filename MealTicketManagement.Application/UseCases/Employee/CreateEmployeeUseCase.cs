a90e67ae33cc3ce9fd86f33a0da33b1a790564a0
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:47:43 -0300

    feat: implementa caso de uso de criação de funcionário

---
+++ MealTicketManagement.Application/UseCases/Employee/CreateEmployeeUseCase.cs
@@ -0,0 +1,28 @@
+﻿using MealTicketManagement.Application.DTOs.Employee;
+using MealTicketManagement.Application.Interfaces;
+using MealTicketManagement.Domain.Exceptions;
+using MealTicketManagement.Domain.Interfaces;
+using EmployeeEntity = MealTicketManagement.Domain.Entities.Employee;
+
+namespace MealTicketManagement.Application.UseCases.Employee;
+
+public class CreateEmployeeUseCase : ICreateEmployeeUseCase
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public CreateEmployeeUseCase(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    public async Task<EmployeeResponse> Execute(CreateEmployeeRequest request)
+    {
+        if (await _employeeRepository.ExistsCpfAsync(request.Cpf))
+            throw new BusinessException("CPF já cadastrado.");
+
+        var employee = new EmployeeEntity(request.Name, request.Cpf);
+        await _employeeRepository.AddAsync(employee);
+
+        return new EmployeeResponse(employee.Id, employee.Name, employee.Cpf, employee.Status, employee.UpdatedAt);
+    }
+}
