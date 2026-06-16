bdbf70a3a7dd22eca2606fe6633b435f379b46fa
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:46:38 -0300

    feat: implementa interface IGetEmployeeByIdUseCase

---
+++ MealTicketManagement.Application/Interfaces/IGetEmployeeByIdUseCase.cs
@@ -0,0 +1,8 @@
+﻿using MealTicketManagement.Application.DTOs.Employee;
+
+namespace MealTicketManagement.Application.Interfaces;
+
+public interface IGetEmployeeByIdUseCase
+{
+    Task<EmployeeResponse> Execute(Guid id);
+}
