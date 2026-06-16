1cbc72f4f90dc467904b128fd56417428b1ab4ce
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:46:15 -0300

    feat: implementa interface ICreateEmployeeUseCase

---
+++ MealTicketManagement.Application/Interfaces/ICreateEmployeeUseCase.cs
@@ -0,0 +1,8 @@
+﻿using MealTicketManagement.Application.DTOs.Employee;
+
+namespace MealTicketManagement.Application.Interfaces;
+
+public interface ICreateEmployeeUseCase
+{
+    Task<EmployeeResponse> Execute(CreateEmployeeRequest request);
+}
