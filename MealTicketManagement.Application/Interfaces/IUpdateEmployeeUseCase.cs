5647709cab360e55b1a0858cf18aee0e1f36914a
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:46:25 -0300

    feat: implementa interface IUpdateEmployeeUseCase

---
+++ MealTicketManagement.Application/Interfaces/IUpdateEmployeeUseCase.cs
@@ -0,0 +1,8 @@
+﻿using MealTicketManagement.Application.DTOs.Employee;
+
+namespace MealTicketManagement.Application.Interfaces;
+
+public interface IUpdateEmployeeUseCase
+{
+    Task<EmployeeResponse> Execute(Guid id, UpdateEmployeeRequest request);
+}
