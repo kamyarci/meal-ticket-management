fbb71e334ee712305552b05c703e2617377d781e
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:49:10 -0300

    feat: implementa caso de uso de busca de todos os funcionários

---
+++ MealTicketManagement.Application/UseCases/Employee/GetAllEmployeesUseCase.cs
@@ -0,0 +1,21 @@
+﻿using MealTicketManagement.Application.DTOs.Employee;
+using MealTicketManagement.Application.Interfaces;
+using MealTicketManagement.Domain.Interfaces;
+
+namespace MealTicketManagement.Application.UseCases.Employee;
+
+public class GetAllEmployeesUseCase : IGetAllEmployeesUseCase
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public GetAllEmployeesUseCase(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    public async Task<IEnumerable<EmployeeResponse>> Execute()
+    {
+        var employees = await _employeeRepository.GetAllAsync();
+        return employees.Select(e => new EmployeeResponse(e.Id, e.Name, e.Cpf, e.Status, e.UpdatedAt));
+    }
+}
