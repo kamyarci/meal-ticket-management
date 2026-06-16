19a7639705a3a4fc3d20c76f90e0b0ae2daffb31
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 18:48:59 -0300

    feat: implementa caso de uso de busca de funcionário por id

---
+++ MealTicketManagement.Application/UseCases/Employee/GetEmployeeByIdUseCase.cs
@@ -0,0 +1,25 @@
+﻿using MealTicketManagement.Application.DTOs.Employee;
+using MealTicketManagement.Application.Interfaces;
+using MealTicketManagement.Domain.Exceptions;
+using MealTicketManagement.Domain.Interfaces;
+
+namespace MealTicketManagement.Application.UseCases.Employee;
+
+public class GetEmployeeByIdUseCase : IGetEmployeeByIdUseCase
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public GetEmployeeByIdUseCase(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    public async Task<EmployeeResponse> Execute(Guid id)
+    {
+        var employee = await _employeeRepository.GetByIdAsync(id);
+        if (employee is null)
+            throw new BusinessException("Funcionário não encontrado.");
+
+        return new EmployeeResponse(employee.Id, employee.Name, employee.Cpf, employee.Status, employee.UpdatedAt);
+    }
+}
