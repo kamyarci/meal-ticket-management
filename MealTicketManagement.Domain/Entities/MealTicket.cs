dc97b619bbbe8b3584eac4a78c7bfe459211dfbf
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 14:58:34 -0300

    feat: adiciona entidade mealTicket

---
+++ MealTicketManagement.Domain/Entities/MealTicket.cs
@@ -0,0 +1,31 @@
+﻿using MealTicketManagement.Domain.Enums;
+
+namespace MealTicketManagement.Domain.Entities;
+
+public class MealTicket
+{
+    public Guid Id { get; private set; }
+    public Employee Employee { get; private set; } = null!;
+    public int Quantity { get; private set; }
+    public Status Status { get; private set; }
+    public DateTime DeliveredAt { get; private set; }
+
+    public MealTicket()
+    {
+    }
+
+    public MealTicket(Employee employee, int quantity)
+    {
+        Id = Guid.NewGuid();
+        Employee = employee;
+        Quantity = quantity;
+        Status = Status.Active;
+        DeliveredAt = DateTime.UtcNow;
+    }
+
+    public void Update(int quantity, Status status)
+    {
+        Quantity = quantity;
+        Status = status;
+    }
+}
