27fe6644a48b6594e2cc0a465487a4d7173973da
Kamyla Arci <kamylaarci@gmail.com>
2026-06-16 16:48:35 -0300

    feat: adiciona interface IEmployeeRepository

---
+++ MealTicketManagement.Domain/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿using MealTicketManagement.Domain.Entities;
+
+namespace MealTicketManagement.Domain.Interfaces;
+
+public interface IEmployeeRepository : IRepository<Employee, Guid>
+{
+    Task<bool> ExistsCpfAsync(string cpf);
+}
