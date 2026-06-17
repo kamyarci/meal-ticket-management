2a7ff7f1abeae9f53732bd6991f7b61af71cda59
Kamyla Arci <kamylaarci@gmail.com>
2026-06-17 11:25:35 -0300

    fix: corrige namespace do UpdateMealTicketRequest

--- MealTicketManagement.Application/DTOs/MealTicket/Request/UpdateMealTicketRequest.cs
+++ MealTicketManagement.Application/DTOs/MealTicket/Request/UpdateMealTicketRequest.cs
@@ -1,5 +1,5 @@
 ﻿using MealTicketManagement.Domain.Enums;
 
-namespace MealTicketManagement.Application.DTOs.MealTicket;
+namespace MealTicketManagement.Application.DTOs.MealTicket.Request;
 
 public record UpdateMealTicketRequest(int Quantity, Status Status);
