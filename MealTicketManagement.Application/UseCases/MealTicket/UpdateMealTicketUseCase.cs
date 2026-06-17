83158607f19a239e17cceed6357c5e997f1b3c4b
Kamyla Arci <kamylaarci@gmail.com>
2026-06-17 11:32:48 -0300

    feat: implementa caso de uso para atualização de tickets de refeição

--- MealTicketManagement.Application/UseCases/MealTicket/UpdateMealTicketUseCase.cs
+++ MealTicketManagement.Application/UseCases/MealTicket/UpdateMealTicketUseCase.cs
@@ -1,6 +1,30 @@
-﻿namespace MealTicketManagement.Application.UseCases.MealTicket;
+﻿using MealTicketManagement.Application.DTOs.MealTicket.Request;
+using MealTicketManagement.Application.DTOs.MealTicket.Response;
+using MealTicketManagement.Application.Interfaces.MealTicket;
+using MealTicketManagement.Domain.Exceptions;
+using MealTicketManagement.Domain.Interfaces;
 
-public class UpdateMealTicketUseCase
+namespace MealTicketManagement.Application.UseCases.MealTicket;
+
+public class UpdateMealTicketUseCase: IUpdateMealTicketUseCase
 {
-    
+    private readonly IMealTicketRepository _mealTicketRepository;
+
+    public UpdateMealTicketUseCase(IMealTicketRepository mealTicketRepository)
+    {
+        _mealTicketRepository = mealTicketRepository;
+    }
+
+    public async Task<MealTicketResponse> Execute(Guid id, UpdateMealTicketRequest request)
+    {
+        var ticketMeal = await _mealTicketRepository.GetByIdAsync(id);
+        if (ticketMeal is null)
+            throw new BusinessException("Ticket não encontrado.");
+
+        ticketMeal.Update(request.Quantity, request.Status);
+        await _mealTicketRepository.UpdateAsync(ticketMeal);
+
+        return new MealTicketResponse(ticketMeal.Id, ticketMeal.Employee.Id, ticketMeal.Employee.Name,
+            ticketMeal.Quantity, ticketMeal.Status, ticketMeal.DeliveredAt);
+    }
 }
