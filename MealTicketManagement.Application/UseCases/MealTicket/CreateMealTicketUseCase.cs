d2c754d170ac6a64d3ea78c8639b1fc382021e93
Kamyla Arci <kamylaarci@gmail.com>
2026-06-17 11:32:35 -0300

    feat: implementa caso de uso para a criação de tickets de refeição

--- MealTicketManagement.Application/UseCases/MealTicket/CreateMealTicketUseCase.cs
+++ MealTicketManagement.Application/UseCases/MealTicket/CreateMealTicketUseCase.cs
@@ -1,6 +1,38 @@
-﻿namespace MealTicketManagement.Application.UseCases.MealTicket;
+﻿using MealTicketManagement.Application.DTOs.MealTicket.Request;
+using MealTicketManagement.Application.DTOs.MealTicket.Response;
+using MealTicketManagement.Application.Interfaces.MealTicket;
+using MealTicketManagement.Domain.Enums;
+using MealTicketManagement.Domain.Exceptions;
+using MealTicketManagement.Domain.Interfaces;
+using MealTicketEntity = MealTicketManagement.Domain.Entities.MealTicket;
 
-public class CreateMealTicketUseCase
+namespace MealTicketManagement.Application.UseCases.MealTicket;
+
+public class CreateMealTicketUseCase : ICreateMealTicketUseCase
 {
-    
+    private readonly IMealTicketRepository _mealTicketRepository;
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public CreateMealTicketUseCase(IMealTicketRepository mealTicketRepository, IEmployeeRepository employeeRepository)
+    {
+        _mealTicketRepository = mealTicketRepository;
+        _employeeRepository = employeeRepository;
+    }
+
+    public async Task<MealTicketResponse> Execute(CreateMealTicketRequest request)
+    {
+        var employee = await _employeeRepository.GetByIdAsync(request.EmployeeId);
+
+        if (employee is null)
+            throw new BusinessException("Funcionário não encontrado.");
+
+        if (employee.Status == Status.Inactive)
+            throw new BusinessException("Não é possível criar ticket para um funcionário inativo.");
+
+        var ticket = new MealTicketEntity(employee, request.Quantity);
+        await _mealTicketRepository.AddAsync(ticket);
+
+        return new MealTicketResponse(ticket.Id, ticket.Employee.Id, ticket.Employee.Name, ticket.Quantity,
+            ticket.Status, ticket.DeliveredAt);
+    }
 }
