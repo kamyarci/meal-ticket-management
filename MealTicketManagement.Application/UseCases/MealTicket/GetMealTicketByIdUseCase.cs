5100cc145f4b651b15a0ffc26250987c3ec32006
Kamyla Arci <kamylaarci@gmail.com>
2026-06-17 11:33:42 -0300

    feat: implementa caso de uso pra buscar os tickets de refeição pelo id

--- MealTicketManagement.Application/UseCases/MealTicket/GetMealTicketByIdUseCase.cs
+++ MealTicketManagement.Application/UseCases/MealTicket/GetMealTicketByIdUseCase.cs
@@ -1,6 +1,26 @@
-﻿namespace MealTicketManagement.Application.UseCases.MealTicket;
+﻿using MealTicketManagement.Application.DTOs.MealTicket.Response;
+using MealTicketManagement.Application.Interfaces.MealTicket;
+using MealTicketManagement.Domain.Exceptions;
+using MealTicketManagement.Domain.Interfaces;
 
-public class GetMealTicketByIdUseCase
+namespace MealTicketManagement.Application.UseCases.MealTicket;
+
+public class GetMealTicketByIdUseCase : IGetMealTicketByIdUseCase
 {
-    
+    private readonly IMealTicketRepository _mealTicketRepository;
+
+    public GetMealTicketByIdUseCase(IMealTicketRepository mealTicketRepository)
+    {
+        _mealTicketRepository = mealTicketRepository;
+    }
+
+    public async Task<MealTicketResponse> Execute(Guid id)
+    {
+        var ticketMeal = await _mealTicketRepository.GetByIdAsync(id);
+        if (ticketMeal is null)
+            throw new BusinessException("Ticket não encontrado.");
+
+        return new MealTicketResponse(ticketMeal.Id, ticketMeal.Employee.Id, ticketMeal.Employee.Name,
+            ticketMeal.Quantity, ticketMeal.Status, ticketMeal.DeliveredAt);
+    }
 }
