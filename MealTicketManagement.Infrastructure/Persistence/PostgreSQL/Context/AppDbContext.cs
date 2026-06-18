75d4f05e450ffdf0f73148f1f3baa349fa3535e6
Kamyla Arci <kamylaarci@gmail.com>
2026-06-18 11:08:52 -0300

    feat: implementa AppDbContext

---
+++ MealTicketManagement.Infrastructure/Persistence/PostgreSQL/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MealTicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<MealTicket> MealTickets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
