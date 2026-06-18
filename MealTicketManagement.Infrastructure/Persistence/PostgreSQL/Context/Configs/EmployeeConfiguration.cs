8ccf743a815f53f1897fe0605ccf6d534b8dec02
Kamyla Arci <kamylaarci@gmail.com>
2026-06-18 11:08:15 -0300

    feat: adiciona configurações de entidade para Employee e MealTicket

---
+++ MealTicketManagement.Infrastructure/Persistence/PostgreSQL/Context/Configs/EmployeeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using MealTicketManagement.Domain.Entities;
+using MealTicketManagement.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Context.Configs;
+
+public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.ToTable("employees");
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id)
+            .ValueGeneratedOnAdd();
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(p => p.Cpf)
+            .IsRequired()
+            .HasMaxLength(11);
+        builder.HasIndex(p => p.Cpf).IsUnique();
+        builder.Property(p => p.Status)
+            .IsRequired()
+            .HasConversion(v => v.ToString()[0].ToString(), v => (Status)v[0])
+            .HasMaxLength(1);
+
+        builder.Property(p => p.UpdatedAt)
+            .IsRequired();
+    }
+}
---
+++ MealTicketManagement.Infrastructure/Persistence/PostgreSQL/Context/Configs/MealTicketConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using MealTicketManagement.Domain.Entities;
+using MealTicketManagement.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Context.Configs;
+
+public class MealTicketConfiguration : IEntityTypeConfiguration<MealTicket>
+{
+    public void Configure(EntityTypeBuilder<MealTicket> builder)
+    {
+        builder.ToTable("meal_tickets");
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id)
+            .ValueGeneratedOnAdd();
+        builder.Property(p => p.Quantity)
+            .IsRequired();
+        builder.Property(p => p.Status)
+            .IsRequired()
+            .HasConversion(v => v.ToString()[0].ToString(), v => (Status)v[0])
+            .HasMaxLength(1);
+        builder.Property(p => p.DeliveredAt)
+            .IsRequired();
+        builder.HasOne(p => p.Employee)
+            .WithMany()
+            .HasForeignKey("EmployeeId")
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
+    }
+}
