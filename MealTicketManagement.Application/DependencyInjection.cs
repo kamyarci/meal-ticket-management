66c2c298d5c8886ae5b0c38fc847a2019ecc8f86
Kamyla Arci <kamylaarci@gmail.com>
2026-06-18 11:12:02 -0300

    feat: adiciona injeção de dependência para serviços de aplicação e infraestrutura

---
+++ MealTicketManagement.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using MealTicketManagement.Application.Interfaces.Employee;
+using MealTicketManagement.Application.Interfaces.MealTicket;
+using MealTicketManagement.Application.UseCases.Employee;
+using MealTicketManagement.Application.UseCases.MealTicket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MealTicketManagement.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<ICreateEmployeeUseCase, CreateEmployeeUseCase>();
+        services.AddScoped<IUpdateEmployeeUseCase, UpdateEmployeeUseCase>();
+        services.AddScoped<IGetEmployeeByIdUseCase, GetEmployeeByIdUseCase>();
+        services.AddScoped<IGetAllEmployeesUseCase, GetAllEmployeesUseCase>();
+
+        services.AddScoped<ICreateMealTicketUseCase, CreateMealTicketUseCase>();
+        services.AddScoped<IUpdateMealTicketUseCase, UpdateMealTicketUseCase>();
+        services.AddScoped<IGetMealTicketByIdUseCase, GetMealTicketByIdUseCase>();
+        services.AddScoped<IGetTicketReportUseCase, GetTicketReportUseCase>();
+
+        return services;
+    }
+}
---
+++ MealTicketManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using MealTicketManagement.Domain.Interfaces;
+using MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Context;
+using MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MealTicketManagement.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IMealTicketRepository, MealTicketRepository>();
+
+        return services;
+    }
+}
