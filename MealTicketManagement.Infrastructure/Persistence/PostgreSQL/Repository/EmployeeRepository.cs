276a754d44faecd13be1702b37365aa096eccc08
Kamyla Arci <kamylaarci@gmail.com>
2026-06-18 11:02:18 -0300

    feat: implementa repositórios de employee e meal tickets

---
+++ MealTicketManagement.Infrastructure/Persistence/PostgreSQL/Repository/EmployeeRepository.cs
@@ -0,0 +1,45 @@
+﻿using MealTicketManagement.Domain.Entities;
+using MealTicketManagement.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Context;
+
+namespace MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Repository;
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public EmployeeRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddAsync(Employee employee)
+    {
+        await _dbContext.Employees.AddAsync(employee);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Employee employee)
+    {
+        _dbContext.Employees.Update(employee);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<Employee?> GetByIdAsync(Guid id)
+    {
+        Employee? employee = await _dbContext.Employees.FindAsync(id);
+        return employee;
+    }
+
+    public async Task<IEnumerable<Employee>> GetAllAsync()
+    {
+        List<Employee> employees = await _dbContext.Employees.ToListAsync();
+        return employees;
+    }
+
+    public async Task<bool> ExistsCpfAsync(string cpf)
+    {
+        return await _dbContext.Employees.AnyAsync(e => e.Cpf == cpf);
+    }
+}
---
+++ MealTicketManagement.Infrastructure/Persistence/PostgreSQL/Repository/MealTicketRepository.cs
@@ -0,0 +1,52 @@
+﻿using MealTicketManagement.Domain.Entities;
+using MealTicketManagement.Domain.Interfaces;
+using MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealTicketManagement.Infrastructure.Persistence.PostgreSQL.Repository;
+
+public class MealTicketRepository : IMealTicketRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public MealTicketRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddAsync(MealTicket mealTicket)
+    {
+        await _dbContext.MealTickets.AddAsync(mealTicket);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(MealTicket mealTicket)
+    {
+        _dbContext.MealTickets.Update(mealTicket);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<MealTicket?> GetByIdAsync(Guid id)
+    {
+        MealTicket? mealTicket = await _dbContext.MealTickets
+            .Include(mt => mt.Employee)
+            .FirstOrDefaultAsync(mt => mt.Id == id);
+        return mealTicket;
+    }
+
+    public async Task<IEnumerable<MealTicket>> GetAllAsync()
+    {
+        List<MealTicket> mealTickets = await _dbContext.MealTickets
+            .Include(mt => mt.Employee)
+            .ToListAsync();
+        return mealTickets;
+    }
+
+    public async Task<IEnumerable<MealTicket>> GetByPeriodAsync(DateTime start, DateTime end)
+    {
+        List<MealTicket> mealTickets = await _dbContext.MealTickets
+            .Include(mt => mt.Employee)
+            .Where(mt => mt.DeliveredAt >= start && mt.DeliveredAt <= end).ToListAsync();
+        return mealTickets;
+    }
+}
